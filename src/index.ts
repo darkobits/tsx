839cddd2235013add4e7f60c36b8de9ed4ce30f5
Joshua <joshua+github@frontlawn.net>
2022-03-10 16:54:43 -0800

    refactor(babel): Remove `babelEsm` configuration.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export { jest } from '@darkobits/ts';
 export { default as nr } from 'config/nr';
 export { default as vite } from 'config/vite';
 export const babel = require.resolve('config/babel');
-export const babelEsm = require.resolve('config/babel-esm');
 export const eslint = require.resolve('config/eslint');
