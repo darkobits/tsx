cd28283a05062e20903e807bef524357d7d59406
Joshua <joshua+github@frontlawn.net>
2022-07-26 17:24:30 -0700

    refactor: Add `generateManualChunksMap` export.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { default as nr } from 'config/nr';
 export { default as vite } from 'config/vite';
 export const babel = require.resolve('config/babel');
 export const eslint = require.resolve('config/eslint');
+
+// Utilities and helpers.
+export { generateManualChunksMap } from 'lib/utils';
