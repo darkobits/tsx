e8fad2b69ef81c058b922692a6218ab304841f25
Joshua <joshua+github@frontlawn.net>
2023-02-20 09:45:50 -0800

    fix: Restore `start` script.

--- src/config/nr.ts
+++ src/config/nr.ts
@@ -6,6 +6,17 @@ import type { ConfigurationFactory } from '@darkobits/nr/dist/etc/types';
 // N.B. This effectively re-exports all scripts defined in `ts` while still
 // allowing `tsx` users to define additional scripts.
 export default (userConfigFactory?: ConfigurationFactory) => nr(async context => {
+  const { command, script } = context;
+
+  script('start', {
+    group: 'Lifecycle',
+    description: 'Start the Vite dev server.',
+    run: [
+      command('vite-serve', ['vite', ['serve']])
+    ]
+  });
+
+
   if (typeof userConfigFactory === 'function') {
     await userConfigFactory(context);
   }
