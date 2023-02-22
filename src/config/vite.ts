5492d8b683fd8aecdc91784a63219c749947094b
Joshua <joshua+github@frontlawn.net>
2023-02-21 19:57:20 -0800

    chore(deps): Update dependencies.

--- src/config/vite.ts
+++ src/config/vite.ts
@@ -1,8 +1,8 @@
 import path from 'path';
 
-import { BARE_EXTENSIONS, TEST_FILE_PATTERNS } from '@darkobits/ts/etc/constants';
+import { interopImportDefault } from '@darkobits/interop-import-default';
 import tscAliasPlugin from '@darkobits/ts/lib/tsc-alias-plugin';
-import { createViteConfigurationPreset, interopRequireDefault } from '@darkobits/ts/lib/utils';
+import { createViteConfigurationPreset } from '@darkobits/ts/lib/utils';
 import typescriptPlugin from '@rollup/plugin-typescript';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import reactPlugin from '@vitejs/plugin-react';
@@ -14,6 +14,7 @@ import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPathsPluginExport from 'vite-tsconfig-paths';
 
 import { IMPORT_META_ENV } from 'etc/constants';
+import log from 'lib/log';
 import {
   gitDescribe,
   createManualChunksHelper,
@@ -25,28 +26,44 @@ import type { ReactPresetContext } from 'etc/types';
 
 
 // Fix default imports from problematic packages.
-const checkerPlugin = interopRequireDefault(checkerPluginExport, 'vite-plugin-checker');
-const tsconfigPathsPlugin = interopRequireDefault(tsconfigPathsPluginExport, 'vite-tsconfig-paths');
+const checkerPlugin = interopImportDefault(checkerPluginExport);
+const tsconfigPathsPlugin = interopImportDefault(tsconfigPathsPluginExport);
 
 
 // ----- React Configuration Preset --------------------------------------------
 
 export const react = createViteConfigurationPreset<ReactPresetContext>(async context => {
-  const { mode, config, root, srcDir, outDir, tsConfigPath } = context;
+  const {
+    root,
+    mode,
+    config,
+    srcDir,
+    outDir,
+    packageJson,
+    tsConfigPath,
+    patterns: {
+      SOURCE_FILES,
+      TEST_FILES
+    }
+  } = context;
 
-  // Assign helpers.
+  // Assign helpers exclusive to ReactPresetContext.
   context.bytes = bytes;
   context.manualChunks = createManualChunksHelper(context);
   context.ms = ms;
   context.reconfigurePlugin = createPluginReconfigurator(context);
   context.useHttpsDevServer = createHttpsDevServerHelper(context);
 
-  // Build pattern for source files and test files.
-  const SOURCE_FILES = [srcDir, '**', `*.{${BARE_EXTENSIONS.join(',')}}`].join(path.sep);
-  const TEST_FILES = [srcDir, '**', `*.{${TEST_FILE_PATTERNS.join(',')}}.{${BARE_EXTENSIONS.join(',')}}`].join(path.sep);
-
   // Global source map setting used by various plug-ins below.
   const sourceMap = true;
+
+
+  // ----- Preflight Checks ----------------------------------------------------
+
+  // Compute anything we need to use async for concurrently.
+  const [eslintConfigResult] = await Promise.all([
+    glob(['.eslintrc.*'], { cwd: root })
+  ]);
 
 
   // ----- Build Configuration -------------------------------------------------
@@ -57,8 +74,10 @@ export const react = createViteConfigurationPreset<ReactPresetContext>(async con
   // Use the inferred output directory defined in tsconfig.json.
   config.build.outDir = path.resolve(root, outDir);
 
+  // Empty the output directory before writing the new compilation to it.
   config.build.emptyOutDir = true;
 
+  // Enable source maps.
   config.build.sourcemap = sourceMap;
 
   config.build.rollupOptions = config.build.rollupOptions ?? {};
@@ -69,10 +88,12 @@ export const react = createViteConfigurationPreset<ReactPresetContext>(async con
   if (Array.isArray(config.build.rollupOptions.output))
     throw new Error('[tsx:react] Expected type of "rollupOptions.output" to be "object", got "Array".');
 
-  config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js';
   config.build.rollupOptions.output.assetFileNames = 'assets/[name]-[hash][extname]';
-  config.build.rollupOptions.output.chunkFileNames =  '[name]-[hash].js';
+  config.build.rollupOptions.output.entryFileNames = '[name]-[hash].js';
+  config.build.rollupOptions.output.chunkFileNames = '[name]-[hash].js';
 
+  // Very simplistic code-splitting strategy that simply puts any module from
+  // the node_modules folder in a "vendor" chunk.
   config.build.rollupOptions.output.manualChunks = (rawId: string) => {
     const id = rawId.replace(/\0/g, '');
     if (id.includes('node_modules')) return 'vendor';
@@ -96,6 +117,8 @@ export const react = createViteConfigurationPreset<ReactPresetContext>(async con
   // ----- Vitest Configuration ------------------------------------------------
 
   config.test = {
+    root,
+    name: packageJson.name,
     environment: 'jsdom',
     deps: {
       interopDefault: true
@@ -110,50 +133,65 @@ export const react = createViteConfigurationPreset<ReactPresetContext>(async con
 
   // ----- Plugin: TypeScript --------------------------------------------------
 
-  // This plugin is responsible for type-checking the project and outputting
-  // declaration files. It reads the project's tsconfig.json automatically,
-  // so the below configuration is only overrides.
+  /**
+   * This plugin is used to emit declaration files _only_. Its error reporting
+   * UX is less than ideal, so we rely on vite-plugin-checker for type-checking.
+   */
   config.plugins.push(typescriptPlugin({
     exclude: [TEST_FILES],
     compilerOptions: {
       // The user should have set either rootDir or baseUrl in their
-      // tsconfig.json, but we actually need both to be set to the same
-      // value to ensure Typescript compiles declarations properly.
+      // tsconfig.json, but we actually need _both_ to be set to the same
+      // value to ensure TypeScript compiles declarations properly.
       rootDir: srcDir,
       baseUrl: srcDir,
-      // Suppresses warnings from the plugin. Because we are only using this
-      // plugin to output declaration files, this setting has no effect on
-      // source output anyway.
+      // This plugin will issue a warning if this is set to any other value.
+      // Because we are only using this plugin to output declaration files, this
+      // setting has no effect on source output.
       module: 'esnext',
       // Ensure we only emit declaration files; all other source should be
       // processed by Vite/Rollup.
       emitDeclarationOnly: true,
       // Do not fail if an error is encountered; vite-plugin-checker will handle
-      // type errors.
+      // error reporting.
       noEmitOnError: false,
-      // If we have build.sourcemap set to `true`, this must also be `true`
-      // or the plugin will issue a warning.
-      sourceMap
+      // If we have `config.build.sourcemap` set to `true`, this must also be
+      // `true` or the plugin will issue a warning.
+      sourceMap: config.build.sourcemap
     }
   }));
 
 
   // ----- Plugin: tsconfig-paths ----------------------------------------------
 
-  // This plugin allows Rollup to resolve import/require statements in
-  // source files by using path mappings configured in the project's
-  // tsconfig.json file.
+  /**
+   * This plugin allows Rollup to resolve import/require specifiers in source
+   * files using path mappings configured in tsconfig.json.
+   *
+   * Note: Because Vite does not process declaration files emitted by
+   * TypeScript, we will need to resolve those import/export specifiers
+   * separately.
+   *
+   * See: https://github.com/aleclarson/vite-tsconfig-paths
+   */
   config.plugins.push(tsconfigPathsPlugin({ root }));
 
 
   // ----- Plugin: tsc-alias ---------------------------------------------------
 
-  // This plugin is responsible for resolving import/export statements to
-  // relative paths in declaration files after the TypeScript compiler has
-  // finished writing them. This is a requirement for the declaration files
-  // to work for consumers, and TypeScript will not resolve these paths on
-  // its own.
-  config.plugins.push(tscAliasPlugin({ configFile: tsConfigPath }));
+  /**
+   * This plugin is responsible for resolving and re-writing import/export
+   * specifiers in emitted declaration files. Note that it _does_ scan the
+   * entire output directory and will also re-write specifiers in emitted source
+   * files, but this operation is redundant; source specifiers will have already
+   * been re-written by tsconfig-paths (see above).
+   *
+   * See: https://github.com/justkey007/tsc-alias
+   */
+  config.plugins.push(tscAliasPlugin({
+    configFile: tsConfigPath,
+    debug: log.isLevelAtLeast('silly')
+  }));
 
 
   // ----- Plugin: React -------------------------------------------------------
@@ -191,13 +229,21 @@ export const react = createViteConfigurationPreset<ReactPresetContext>(async con
 
   // ----- Plugin: Checker -----------------------------------------------------
 
-  const hasEslintConfig = (await glob(['.eslintrc.*'], { cwd: root })).length > 0;
+  const hasEslintConfig = eslintConfigResult.length > 0;
 
+  /**
+   * This plugin is responsible for type-checking and linting the project. It
+   * runs each checker in a worker thread to speed up build times, and uses
+   * nice overlays with the Vite dev server. However, it is not as configurable
+   * as @rollup/plugin-typescript, so we still need the latter to properly
+   * generate declaration files.
+   *
+   * See: https://github.com/fi3ework/vite-plugin-checker
+   */
   config.plugins.push(checkerPlugin({
     typescript: true,
-    eslint: hasEslintConfig
-      ? {
-        lintCommand: `eslint "${SOURCE_FILES}"`
-      } : false
+    eslint: hasEslintConfig && mode !== 'test'
+      ? { lintCommand: `eslint "${SOURCE_FILES}"` }
+      : false
   }));
 });
