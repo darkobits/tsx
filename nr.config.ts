db54d766f8dc3a183f78e008bd0482f675ee9aad
Joshua <joshua+github@frontlawn.net>
2023-02-21 19:57:33 -0800

    chore: Update package scripts.

--- nr.config.ts
+++ nr.config.ts
@@ -1,12 +1,42 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 import { nr } from '@darkobits/ts';
 
 
-export default nr(({ script, command }) => {
+export default nr(({ script, command, task }) => {
+  // When publishing this package, we use re-pack's 'publish' command to publish
+  // from the .re-pack folder rather than `npm publish`.
   script('publish', {
     group: 'Release',
     description: 'Publish the package using re-pack.',
     run: [
-      command('re-pack', ['re-pack', ['publish']])
+      // Re-pack the project.
+      command('re-pack', ['re-pack']),
+      // Publish the project from the re-pack directory.
+      command('re-pack-publish', ['re-pack', ['publish']]),
+      // Push the release commit.
+      command('git-push', ['git', ['push', 'origin', 'HEAD'], {
+        setUpstream: true,
+        followTags: true
+      }]),
+      // Remove the re-pack directory.
+      task('rm-re-pack', () => fs.rm(path.resolve('.re-pack'), {
+        recursive: true,
+        force: true
+      }))
+    ]
+  });
+
+  script('postBump', {
+    group: 'Lifecycles',
+    description: 'Publishes the project and pushes the release commit.',
+    run: [
+      'script:publish',
+      command('git-push', ['git', ['push', 'origin', 'HEAD'], {
+        followTags: true,
+        setUpstream: true
+      }])
     ]
   });
 });
