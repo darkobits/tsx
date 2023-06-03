b33b1d91b100fea96fb2efa8fedbdd9f3733dff8
Joshua <joshua+github@frontlawn.net>
2023-06-03 06:13:51 -0700

    chore(deps): Update dependencies.

--- src/config/nr.ts
+++ src/config/nr.ts
@@ -1,11 +1,12 @@
 import { nr } from '@darkobits/ts';
 
-import type { ConfigurationFactory } from '@darkobits/nr/dist/etc/types';
+import type { ConfigurationFactory } from '@darkobits/nr';
 
 
 export default (userConfigFactory?: ConfigurationFactory) => nr(async context => {
   const { command, script } = context;
 
+  // Define a "start" script for consumers of this package who use "nr".
   script('start', {
     group: 'Lifecycle',
     description: 'Start the Vite dev server.',
